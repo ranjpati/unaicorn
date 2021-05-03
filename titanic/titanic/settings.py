55462df831107791f6d77925a0d2346a5b8df86d
ranjpati <ranjpati2010@gmail.com>
2021-05-03 07:37:16 +0530

    Added and integrated CSS and JS into index.html

--- titanic/titanic/settings.py
+++ titanic/titanic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0wcaa&o)y_68i30n7a^4n32!zx5lpkohp$+z_m8tw-oc9fvfa#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] #'ranjitpatilmlai.fun','www.ranjitpatilmlai.fun'
 
 
 # Application definition
@@ -116,9 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os.path
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (os.path.join('static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
